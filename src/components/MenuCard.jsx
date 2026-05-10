b0fe67741f87f02b8e274460ec67b970ce7a9aa4
Pranav-Rastogi1 <pranavrastogi480@gmail.com>
2026-05-10 22:39:45 +0530

    Updating MenuCard and RestInfo

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -2,14 +2,45 @@ import React from 'react'
 import RestInfo from './RestInfo';
 
 const MenuCard = ({item}) => {
+  const [isOpen,setIsOpen] = React.useState(true);
+
+  if("categories" in item){
+    return (
+      <div className='w-full'>
+        <h1 className='text-3xl font-bold mb-4'>{item?.title}</h1>
+        <div>
+          {
+              item?.categories?.map((menuItem)=><MenuCard key={menuItem?.title} item={menuItem}/>)
+          }
+        </div>
+      </div>
+    )
+  }
+
+  if(!isOpen){
+    return (
+      <div className='w-full'>
+      <div className='flex justify-between items-center w-full'>
+      <p className='text-3xl font-bold mb-4'>{item?.title}</p>
+      <button className='text-3xl font-bold mr-40' onClick={() => setIsOpen(!isOpen)}>{isOpen?'˅':'˄'}</button>
+      </div>
+      <div className='h-5 bg-gray-200 mt-2 mb-2'></div>
+    </div>
+    )
+  }
+
   return (
     <div className='w-full'>
-      <h1>{item?.title}</h1>
+      <div className='flex justify-between items-center w-full'>
+      <p className='text-3xl font-bold mb-4'>{item?.title}</p>
+      <button className='text-3xl font-bold mr-40' onClick={() => setIsOpen(!isOpen)}>{isOpen?'˅':'˄'}</button>
+      </div>
       <div>
         {
             item?.itemCards?.map((menuItem)=><RestInfo key={menuItem?.card?.info?.id} restData={menuItem?.card?.info}/>)
         }
       </div>
+      <div className='h-5 bg-gray-200 mt-2 mb-2'></div>
     </div>
   )
 }
--- src/components/RestInfo.jsx
+++ src/components/RestInfo.jsx
@@ -3,20 +3,21 @@ import React from 'react'
 const RestInfo = ({restData}) => {
   return (
     <>
-        <div className='flex w-full justify-center'>
+        <div className='flex w-full justify-center mb-2 pb-2'>
             <div className='w-[70%]'>
-                <p>{restData?.name}</p>
-                <p>{"$" + restData?.defaultPrice/100}</p>
-                <span>{restData?.ratings?.aggregatedRating?.rating}</span>
-                <span>{"(" + restData?.ratings?.aggregatedRating?.ratingCountV2 + ")"}</span>
-                <p>{restData?.description}</p>
+                <p className='text-2xl text-gray-700 font-semibold mb-1'>{restData?.name}</p>
+                <p className='text-xl font-bold'>{"₹" + (("defaultPrice" in restData ? restData.defaultPrice : restData?.price) / 100).toFixed(2)}</p>
+                <span className='text-lg text-green-700 font-bold'>{restData?.ratings?.aggregatedRating?.rating}</span>
+                <span className='text-gray-500'>( {restData?.ratings?.aggregatedRating?.ratingCountV2} )</span>
+                <p className='text-gray-600'>{restData?.description}</p>
 
             </div>
             <div className='w-[20%] relative'>
-                <img className='w-full h-36 object-cover' src={"https://media-assets.swiggy.com/swiggy/image/upload/" + restData?.imageId} alt="" />
-                <button className='absolute bottom-0 left-0 text-green-600 px-4 py-2 bg-white'>ADD</button>
+                <img className='w-full h-36 object-cover rounded-3xl' src={"https://media-assets.swiggy.com/swiggy/image/upload/" + restData?.imageId} alt="" />
+                <button className='absolute bottom-1 rounded-xl left-20 text-2xl font-bold text-green-700 px-2 py-1 bg-white'>ADD</button>
             </div>
         </div>
+        <hr className='mb-4'/>
     </>
   )
 }
